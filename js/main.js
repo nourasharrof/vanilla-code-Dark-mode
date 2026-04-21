0261c0acb7249943000414aa5d374c7c3b0cebdf
nourasharrof <167745712+nourasharrof@users.noreply.github.com>
2026-04-21 23:32:34 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const btn = document.getElementById("toggle");
+
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+}
+
+btn.onclick = () => {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+};
